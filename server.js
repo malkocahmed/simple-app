ca83e6062cf4aadc047a9df604a7b2a6e693ade8
malkocahmed <malkocahmed@gmail.com>
2019-07-08 12:26:34 +0200

    Deploying node app

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("Server is listening...");
 });
